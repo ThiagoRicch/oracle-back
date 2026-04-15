ecca51356df4c3ab82dcf725fc7d1a9af8c4e8f2
ThiagoRicch <tigerthiago56@gmail.com>
2026-04-15 01:26:27 -0300

    feat: add requirements and configure cors for render deploy

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from Controller.ServidorController import router as servidor_router
 from Controller.AuthController import router as auth_router
 from Controller.GeografiaController import router as geografia_router
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -10,9 +14,11 @@ app.include_router(auth_router)
 app.include_router(servidor_router)
 app.include_router(geografia_router)
 
+frontend_url = os.getenv("FRONTEND_URL", "http://localhost:5173")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # em produção coloca a URL do frontend
+    allow_origins=[frontend_url, "http://localhost:5173"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
